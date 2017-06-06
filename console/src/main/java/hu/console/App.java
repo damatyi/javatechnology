6809d9cf3bbaa75e76c656a3472da6becd62ff47
damatyi <damatyi@gmail.com>
2017-06-06 23:03:11 +0200

    log4j
    logger added to app

--- console/src/main/java/hu/console/App.java
+++ console/src/main/java/hu/console/App.java
@@ -5,6 +5,8 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
+import org.apache.log4j.Logger;
+
 import com.fasterxml.jackson.core.JsonParseException;
 import com.fasterxml.jackson.databind.JsonMappingException;
 //import com.fasterxml.jackson.databind.ObjectMapper;
@@ -17,6 +19,7 @@ import hu.service.impl.CardManagementServiceImpl;
 
 public class App 
 {
+	private static final Logger Log = Logger.getLogger(App.class);
 	private static CardManagementService cardManager;
 	
 	static {
@@ -25,12 +28,14 @@ public class App
 	}
     public static void main( String[] args ) throws IOException
     {
+    	Log.info("Application starts");
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		boolean run = true;
 		while (run) {
 			
 			String line = br.readLine();
 			if ("exit".equals(line)) {
+				Log.info("Application exited by user");
 				break;
 			}
 			if ("list sim".equals(line)) {
@@ -59,6 +64,7 @@ public class App
 //		String jsonInString = "{\"pin1\":123,\"pin2\":1234,\"puk1\":654987,\"puk2\":456789}";
 //		Simcard sim1 = mapper.readValue(jsonInString, Simcard.class);
 //		System.out.println(sim1);
+		Log.info("SIM card listed");
 	}
 
 	private static void addCard() throws IOException {
@@ -68,25 +74,30 @@ public class App
 		while (pin1 < 1 | pin1 > 9999){
 			System.out.println("PIN1: ");
 			pin1 = Integer.parseInt(br.readLine());
+				if (pin1 < 1 | pin1 > 9999){Log.info("Wrong SIM data added");}
 			}
 		int pin2 = 0;
 		while (pin2 < 1 | pin2 > 9999){
 			System.out.println("PIN2: ");
 			pin2 = Integer.parseInt(br.readLine());
+			if (pin2 < 1 | pin2 > 9999){Log.info("Wrong SIM data added");}			
 			}
 		int puk1 = 0;
-		while (puk1 < 0 | puk1 > 99999999){
+		while (puk1 < 1 | puk1 > 99999999){
 			System.out.println("PUK1: ");
 			puk1 = Integer.parseInt(br.readLine());
+			if (puk1 < 1 | puk1 > 9999){Log.info("Wrong SIM data added");}			
 			}
 		int puk2 = 0;
-		while (puk2 < 0 | puk2 > 99999999){
+		while (puk2 < 1 | puk2 > 99999999){
 			System.out.println("PUK2: ");
 			puk2 = Integer.parseInt(br.readLine());
+			if (puk2 < 1 | puk2 > 9999){Log.info("Wrong SIM data added");}						
 			}	
 		
 		Simcard simcard = new Simcard(pin1,pin2,puk1,puk2);
 		cardManager.acquireSimcard(simcard);
+		Log.info("SIM card added");
 
 	}
 }
